c8489f27a7a759d99db15325779c407f387615d5
Grzegorz Hawliczek <ghawliczek@outlook.com>
2024-06-10 18:32:09 +0200

    chore: bootstrap application

---
+++ README.md
@@ -0,0 +1,2 @@
+# frontend-react
+This repository contains a frontend React application for [offnd.at](https://offnd.at)
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+export const App = () => {
+  return (
+    <h1 className='text-red-500 text-8xl font-bold underline'>Hello world!</h1>
+  )
+}
