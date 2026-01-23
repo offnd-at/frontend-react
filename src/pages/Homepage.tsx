3c52be35ac2a25cf176937f8bd4541282f885a99
Grzegorz Hawliczek <ghawliczek@users.noreply.github.com>
2026-01-23 23:16:22 +0100

    chore: add SEO and metadata management (#19)

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,20 @@
-import { useEffect } from 'react'
-
 import { LinkGenerator } from '../components/links/LinkGenerator'
 
 export function Homepage() {
-  useEffect(() => {
-    document.title = `offnd.at - share the offensiveness`
-  }, [])
-
-  return <LinkGenerator />
+  return (
+    <>
+      <title>offnd.at - share the offensiveness</title>
+      <meta
+        name='description'
+        content="The world's most offensive URL shortener. Turn boring links into memorable, profanity-laced phrases."
+      />
+      <meta property='og:title' content='offnd.at - share the offensiveness' />
+      <meta
+        property='og:description'
+        content='Turn boring links into memorable, profanity-laced phrases. Share the offensiveness.'
+      />
+      <meta property='og:type' content='website' />
+      <LinkGenerator />
+    </>
+  )
 }
